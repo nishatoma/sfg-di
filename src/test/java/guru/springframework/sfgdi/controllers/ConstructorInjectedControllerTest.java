97d8866837f1a8216b2ecfa739486d285e293e1e
nishatoma <nishatoma@gmail.com>
2020-07-11 20:00:24 -0400

    gg

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController constructorInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        constructorInjectedController = new ConstructorInjectedController(new ConstructorGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+
+        System.out.println(constructorInjectedController.getGreeting());
+    }
+}
